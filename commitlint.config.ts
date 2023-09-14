8224f4901f9516ef52236bd1a2d42a598bf05994
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-14 10:19:24 -0300

    ci: update commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
@@ -18,7 +20,7 @@ module.exports = {
         'chore',
       ],
     ],
-    'scope-enum': [
+    'enhanced-scope-enum': [
       2,
       'always',
       ['package/config', 'package/eslint', 'package/tsconfig', 'package/ui'],
@@ -27,4 +29,40 @@ module.exports = {
     'header-max-length': [2, 'always', 120],
     'scope-min-length': [2, 'always', 4],
   },
+  plugins: [
+    {
+      rules: {
+        'enhanced-scope-enum': (parsed, when = 'always', value = []) => {
+          if (!parsed.scope) {
+            return [true, ''];
+          }
+
+          // only use comma sign as seperator
+          const scopeSegments = parsed.scope.split(',');
+
+          const check = (value, enums) => {
+            if (value === undefined) {
+              return false;
+            }
+            if (!Array.isArray(enums)) {
+              return false;
+            }
+            return enums.indexOf(value) > -1;
+          };
+
+          const negated = when === 'never';
+          const result =
+            value.length === 0 ||
+            scopeSegments.every((scope) => check(scope, value));
+
+          return [
+            negated ? !result : result,
+            `scope must ${negated ? `not` : null} be one of [${value.join(
+              ', '
+            )}]`,
+          ];
+        },
+      },
+    },
+  ],
 };

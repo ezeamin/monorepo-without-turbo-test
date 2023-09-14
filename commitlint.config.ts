36cce7770b4d7d0965a80cb3cc14b755f4f4679b
Ezequiel Amin <ezequielamin@outlook.com>
2023-09-14 10:12:55 -0300

    ci: update commitlint

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -18,7 +18,13 @@ module.exports = {
         'chore',
       ],
     ],
+    'scope-enum': [
+      2,
+      'always',
+      ['package/config', 'package/eslint', 'package/tsconfig', 'package/ui'],
+    ],
     'header-min-length': [2, 'always', 10],
     'header-max-length': [2, 'always', 120],
+    'scope-min-length': [2, 'always', 4],
   },
 };

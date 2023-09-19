1c35e91d3dbdfba108e982f5b2698b36d92e02eb
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-09-19 13:26:40 -0300

    feat(package/eslint): eslint in progress

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -1,17 +1,17 @@
-const { resolve } = require('node:path');
+// const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), 'tsconfig.json');
+// const project = resolve(process.cwd(), 'tsconfig.json');
 
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-    'airbnb',
-    'airbnb-typescript',
+
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:prettier/recommended',
+    'plugin:import/react',
+    'prettier',
+    require.resolve('./rules/react'),
+    // require.resolve('./rules/jsx-a11y'),
   ].map(require.resolve),
   globals: {
     JSX: true,
@@ -21,31 +21,31 @@ module.exports = {
     project,
   },
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    // 'react-refresh/only-export-components': [
+    //   'warn',
+    //   { allowConstantExport: true },
+    // ],
 
-    'no-param-reassign': 'off',
-    'import/prefer-default-export': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/function-component-definition': 'off',
-    '@typescript-eslint/no-empty-interface': 'off',
-    '@typescript-eslint/no-unused-vars': 'error',
+    // 'no-param-reassign': 'off',
+    // 'import/prefer-default-export': 'off',
+    // 'react/react-in-jsx-scope': 'off',
+    // 'react/function-component-definition': 'off',
+    // '@typescript-eslint/no-empty-interface': 'off',
+    // '@typescript-eslint/no-unused-vars': 'error',
 
-    'import/extensions': 'off', // no extensions are needed in import statements
-    'react/jsx-props-no-spreading': 'off',
-    'import/order': 'off', // customized order in .prettierrc
-    'func-names': 'off', // disabled to allow IIFEs
-    'import/no-extraneous-dependencies': 'off', // disabled to allow devDependencies
-    'react/require-default-props': 'off', // deprecated check
+    // 'import/extensions': 'off', // no extensions are needed in import statements
+    // 'react/jsx-props-no-spreading': 'off',
+    // 'import/order': 'off', // customized order in .prettierrc
+    // 'func-names': 'off', // disabled to allow IIFEs
+    // 'import/no-extraneous-dependencies': 'off', // disabled to allow devDependencies
+    // 'react/require-default-props': 'off', // deprecated check
   },
   settings: {
-    'import/resolver': {
-      typescript: {
-        project,
-      },
-    },
+    // 'import/resolver': {
+    //   typescript: {
+    //     project,
+    //   },
+    // },
     react: {
       version: 'detect',
     },

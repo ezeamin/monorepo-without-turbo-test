c716a3048fe656a58c7e8a8a96623f239c75325d
Ignacio Bockl <ignaciobockl96@gmail.com>
2023-09-20 12:06:00 -0300

    feat(package/eslint): eslint configured and working

--- packages/eslint-config-custom/react-internal.js
+++ packages/eslint-config-custom/react-internal.js
@@ -1,44 +1,48 @@
-// const { resolve } = require('node:path');
+const { resolve } = require('node:path');
 
-// const project = resolve(process.cwd(), 'tsconfig.json');
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 module.exports = {
   extends: [
-
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:import/react',
-    'prettier',
-    require.resolve('./rules/react'),
-    // require.resolve('./rules/jsx-a11y'),
+    "@vercel/style-guide/eslint/node" ,
+    '@vercel/style-guide/eslint/react',
+    "@vercel/style-guide/eslint/typescript",
+    // require.resolve('./rules/react'),
   ].map(require.resolve),
   globals: {
     JSX: true,
   },
-  // ignorePatterns: ['node_modules/', 'dist/', '.eslintrc.js'],
   parserOptions: {
     project,
   },
   rules: {
-    // 'react-refresh/only-export-components': [
-    //   'warn',
-    //   { allowConstantExport: true },
-    // ],
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
 
-    // 'no-param-reassign': 'off',
-    // 'import/prefer-default-export': 'off',
-    // 'react/react-in-jsx-scope': 'off',
-    // 'react/function-component-definition': 'off',
-    // '@typescript-eslint/no-empty-interface': 'off',
-    // '@typescript-eslint/no-unused-vars': 'error',
+    'no-param-reassign': 'off',
 
-    // 'import/extensions': 'off', // no extensions are needed in import statements
-    // 'react/jsx-props-no-spreading': 'off',
-    // 'import/order': 'off', // customized order in .prettierrc
-    // 'func-names': 'off', // disabled to allow IIFEs
-    // 'import/no-extraneous-dependencies': 'off', // disabled to allow devDependencies
-    // 'react/require-default-props': 'off', // deprecated check
+    'import/prefer-default-export': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    'import/extensions': 'off', // no extensions are needed in import statements
+    'react/jsx-props-no-spreading': 'off',
+    'import/order': 'off', // customized order in .prettierrc
+    'func-names': 'off', // disabled to allow IIFEs
+    'import/no-extraneous-dependencies': 'off', // disabled to allow devDependencies
+    'react/require-default-props': 'off', // deprecated check
+
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     // 'import/resolver': {
--- packages/ui/.eslintrc.js
+++ packages/ui/.eslintrc.js
@@ -1,15 +1,3 @@
-
 module.exports = {
-  extends: [
-    'eslint-config-custom/react-internal',
-    // 'eslint:recommended',
-    // 'plugin:@typescript-eslint/recommended',
-    // 'plugin:react-hooks/recommended',
-    // 'airbnb',
-    // 'airbnb-typescript',
-    // 'plugin:react/recommended',
-    // 'plugin:jsx-a11y/recommended',
-    // 'plugin:prettier/recommended',
-  ],
+  extends: ['custom/react-internal'],
 };
-

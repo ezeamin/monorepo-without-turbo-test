c664c7b7273f11f981e8e185f06344f2c4857a11
Ezequiel Amin <ezequielamin@outlook.com>
2023-10-24 09:26:10 -0300

    feat(packages/ui): add skeleton component

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,7 +1,8 @@
 import Button from './src/components/Button/Button';
 import IconButton from './src/components/IconButton/IconButton';
+import Skeleton from './src/components/Skeleton/Skeleton';
 
 import 'global-styles/global.css';
 
 // component exports
-export { Button, IconButton };
+export { Button, IconButton, Skeleton };

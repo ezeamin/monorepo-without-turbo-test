9d3c79d0d2c74ab555ea8e281cc110fcf9115aba
Ezequiel Amin <ezequielamin@outlook.com>
2023-11-03 11:26:52 -0300

    fix(packages/puas-components): remove rounded borders on logo

--- packages/puas-components/src/components/Menu/LogoPjt.tsx
+++ packages/puas-components/src/components/Menu/LogoPjt.tsx
@@ -11,10 +11,10 @@ const LogoPjt = (): JSX.Element => {
     <Link data-testid={DTI(DTI_LIST.MENU.LINK(0))} href="/">
       <Image
         alt="Poder Judicial de Tucumán"
-        className="bg-white border-none cursor-pointer mt-4 p-1 rounded-full shadow"
+        className="bg-white border-none cursor-pointer mt-4 p-1 rounded-md"
         data-testid={DTI(DTI_LIST.MENU.LOGO)}
         height={50}
-        // DO NOT delete, it WILL throw an ESLint error
+        // ! DO NOT delete, it WILL throw an ESLint error on linting
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- logo is not a component
         src={typeof logo === 'object' && 'src' in logo ? logo.src : ''}
         width={50}
